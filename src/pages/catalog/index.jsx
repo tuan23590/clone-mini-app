63dbbbc28b85ad19bae43fcdf05726bba77d2c18
tuan3110 <chodai23590@gmail.com>
2024-12-16 11:55:27 +0700

    feat(category-list): implement category list page with search and navigation

--- src/pages/catalog/index.jsx
+++ src/pages/catalog/index.jsx
@@ -1,5 +1,37 @@
+import { useNavigate } from "react-router-dom";
+import { useStore } from "../../store";
+import SearchBar from "../../components/search-bar";
+import CategoryTabs from "../../components/category-tabs";
+import TransitionLink from "../../components/transition-link";
+
 export default function CategoryListPage() {
+  const navigate = useNavigate();
+  const [categories,_] = useStore.categories();
+
   return (
-    <div>CategoryListPage</div>
-  )
+    <>
+      <div className="py-2">
+        <SearchBar onClick={() => navigate("/search")} />
+      </div>
+      <CategoryTabs />
+      <div className="grid grid-cols-4 p-4 gap-x-4 gap-y-8">
+        {categories.map((category) => (
+          <TransitionLink
+            key={category.id}
+            className="flex flex-col items-center space-y-2 overflow-hidden cursor-pointer"
+            to={`/category/${category.id}`}
+          >
+            <img
+              src={category.image}
+              className="aspect-square object-cover rounded-full border-[0.5px] border-black/15"
+              alt={category.name}
+            />
+            <div className="text-center text-sm w-full line-clamp-2 text-subtitle">
+              {category.name}
+            </div>
+          </TransitionLink>
+        ))}
+      </div>
+    </>
+  );
 }
