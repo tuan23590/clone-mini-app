e8f933f117250e5799b11e0acd709a7f4428b1a1
tuan3110 <chodai23590@gmail.com>
2024-12-16 12:02:53 +0700

    feat(profile): implement profile page with actions, points display, and follow widget

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,5 +1,13 @@
+import ProfileActions from "./actions";
+import FollowOAWidget from "./follow-oa";
+import Points from "./points";
+
 export default function ProfilePage() {
   return (
-    <div>ProfilePage</div>
-  )
+    <div className="min-h-full bg-section p-4 space-y-2.5">
+      <Points />
+      <ProfileActions />
+      <FollowOAWidget />
+    </div>
+  );
 }
